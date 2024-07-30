22e0c40ef0f692c61544daf44db883c2fa3407fe
Gustavo Quintana Hidalgo <96747228+gqh22@users.noreply.github.com>
2024-07-30 19:36:05 -0300

    fix js

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content h1, .about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p, .about-content',{origin:'rigth'});
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text',{
-    strings:['Software Developer','Back-End Developer'],
+    strings:['Desarrollador de Software','Semi-Senior'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
